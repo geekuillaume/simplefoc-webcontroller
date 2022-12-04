5d9013132e71f51dc9330a7476b5bde00a65ee7e
Guillaume Besson <guillaume@besson.co>
2022-12-04 22:36:27 +0100

    Remove menu icon in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header(props: any) {
     <Box sx={{ flexGrow: 1 }} {...props}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -20,7 +20,7 @@ export default function Header(props: any) {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             SimpleFoc WebController
           </Typography>
