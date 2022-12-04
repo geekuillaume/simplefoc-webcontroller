f61f174a82efc4e778740a6cbe54175022cd9d18
Guillaume Besson <guillaume@besson.co>
2022-12-04 22:32:49 +0100

    Add message about web serial compatible

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { SimpleFocSerialPort } from "./simpleFoc/serial";
 import Header from "./components/Header";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { SerialManager } from "./components/SerialManager";
 import { Container } from "@mui/system";
 import { Motors } from "./components/Motors";
@@ -10,6 +10,8 @@ import { serialPortContext } from "./lib/serialContext";
 
 function App() {
   const [serial, setSerial] = useState<SimpleFocSerialPort | null>(null);
+
+  const supportSerial = typeof navigator.serial === "object";
 
   return (
     <Box
@@ -21,12 +23,22 @@ function App() {
       }}
     >
       <Header />
+
       <Container maxWidth="lg">
         <Stack gap={3} paddingTop={3}>
-          <serialPortContext.Provider value={serial}>
-            <SerialManager onSetSerial={setSerial} serial={serial} />
-            <Motors />
-          </serialPortContext.Provider>
+          {!supportSerial && (
+            <Typography>
+              WebSerial is not available on your browser, please use a{" "}
+              <a href="https://caniuse.com/web-serial">browser supporting it</a>
+              .
+            </Typography>
+          )}
+          {supportSerial && (
+            <serialPortContext.Provider value={serial}>
+              <SerialManager onSetSerial={setSerial} serial={serial} />
+              <Motors />
+            </serialPortContext.Provider>
+          )}
         </Stack>
       </Container>
     </Box>
